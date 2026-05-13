05d31bb8cb0baeda3170dc63fc907f2f2393dc68
Misaf <misaf.1990@gmail.com>
2026-05-13 13:08:31 +0330

    feat: add German translations

---
+++ resources/lang/de/navigation.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'permission'            => 'Berechtigungen',
+    'permission_management' => 'Berechtigungsverwaltung',
+    'role'                  => 'Rollen',
+];
---
+++ resources/lang/de/table.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'columns' => [
+        'created_at' => 'Erstellt am',
+        'guard_name' => 'Guard-Name',
+        'name'       => 'Name',
+        'role'       => 'Rollen',
+        'updated_at' => 'Aktualisiert am',
+    ],
+    'groups' => [
+        'guard' => 'Guard',
+    ],
+];
