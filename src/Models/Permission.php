205d0126cc3566d5d2a71b2e691fcdb5b8cd8bdb
Misaf <misaf.1990@gmail.com>
2026-06-06 13:28:38 +0330

    closed #2, closed #3: Laravel 13: Replace fillable, hidden with PHP Attributes

--- src/Models/Permission.php
+++ src/Models/Permission.php
@@ -4,52 +4,51 @@ declare(strict_types=1);
 
 namespace Misaf\VendraPermission\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Misaf\VendraActivityLog\Concerns\HasDefaultActivityLogOptions;
 use Misaf\VendraPermission\Database\Factories\PermissionFactory;
 use Misaf\VendraTenant\Traits\BelongsToTenant;
-use Spatie\Activitylog\LogOptions;
 use Spatie\Activitylog\Traits\LogsActivity;
 use Spatie\Permission\Models\Permission as SpatiePermission;
 
 /**
  * @property int $id
  * @property int $tenant_id
+ * @property string $name
  * @property string|null $description
+ * @property string $guard_name
  */
+#[Fillable(['name', 'description', 'guard_name'])]
+#[Hidden(['tenant_id'])]
 final class Permission extends SpatiePermission
 {
     use BelongsToTenant;
+    use HasDefaultActivityLogOptions;
 
     /** @use HasFactory<PermissionFactory> */
     use HasFactory;
 
     use LogsActivity;
 
-    protected $casts = [
-        'id'          => 'integer',
-        'tenant_id'   => 'integer',
-        'name'        => 'string',
-        'description' => 'string',
-        'guard_name'  => 'string',
-    ];
-
-    protected $fillable = [
-        'name',
-        'description',
-        'guard_name',
-    ];
-
-    protected $hidden = [
-        'tenant_id',
-    ];
+    /**
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'id'          => 'integer',
+            'tenant_id'   => 'integer',
+            'name'        => 'string',
+            'description' => 'string',
+            'guard_name'  => 'string',
+        ];
+    }
 
     protected static function newFactory()
     {
         return PermissionFactory::new();
     }
 
-    public function getActivitylogOptions(): LogOptions
-    {
-        return LogOptions::defaults()->logFillable()->logExcept(['id']);
-    }
 }
--- src/Models/Role.php
+++ src/Models/Role.php
@@ -4,52 +4,51 @@ declare(strict_types=1);
 
 namespace Misaf\VendraPermission\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Misaf\VendraActivityLog\Concerns\HasDefaultActivityLogOptions;
 use Misaf\VendraPermission\Database\Factories\RoleFactory;
 use Misaf\VendraTenant\Traits\BelongsToTenant;
-use Spatie\Activitylog\LogOptions;
 use Spatie\Activitylog\Traits\LogsActivity;
 use Spatie\Permission\Models\Role as SpatieRole;
 
 /**
  * @property int $id
  * @property int $tenant_id
+ * @property string $name
  * @property string|null $description
+ * @property string $guard_name
  */
+#[Fillable(['name', 'description', 'guard_name'])]
+#[Hidden(['tenant_id'])]
 final class Role extends SpatieRole
 {
     use BelongsToTenant;
+    use HasDefaultActivityLogOptions;
 
     /** @use HasFactory<RoleFactory> */
     use HasFactory;
 
     use LogsActivity;
 
-    protected $casts = [
-        'id'          => 'integer',
-        'tenant_id'   => 'integer',
-        'name'        => 'string',
-        'description' => 'string',
-        'guard_name'  => 'string',
-    ];
-
-    protected $fillable = [
-        'name',
-        'description',
-        'guard_name',
-    ];
-
-    protected $hidden = [
-        'tenant_id',
-    ];
+    /**
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'id'          => 'integer',
+            'tenant_id'   => 'integer',
+            'name'        => 'string',
+            'description' => 'string',
+            'guard_name'  => 'string',
+        ];
+    }
 
     protected static function newFactory()
     {
         return RoleFactory::new();
     }
 
-    public function getActivitylogOptions(): LogOptions
-    {
-        return LogOptions::defaults()->logFillable()->logExcept(['id']);
-    }
 }
