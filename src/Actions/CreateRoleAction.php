ac38f4de658dd0b46f7f61b67d9e606f24b72edd
Misaf <misaf.1990@gmail.com>
2026-06-13 13:11:30 +0330

    temp

--- src/Actions/CreateRoleAction.php
+++ src/Actions/CreateRoleAction.php
@@ -6,29 +6,21 @@ namespace Misaf\VendraPermission\Actions;
 
 use Misaf\VendraPermission\Models\Role;
 use Misaf\VendraTenant\Models\Tenant;
-use Spatie\Permission\PermissionRegistrar;
 
 final class CreateRoleAction
 {
-    public function __construct(private readonly PermissionRegistrar $permissionRegistrar) {}
-
     public function execute(
         Tenant $tenant,
         string $name,
         ?string $description,
         string $guardName,
     ): Role {
-        $role = $tenant->roles()->firstOrCreate(
-            [
-                'name'       => $name,
-                'guard_name' => $guardName,
-            ],
-            [
-                'description' => $description,
-            ],
-        );
-
-        $this->permissionRegistrar->forgetCachedPermissions();
+        /** @var Role $role */
+        $role = $tenant->execute(static fn() => Role::create([
+            'name'        => $name,
+            'description' => $description,
+            'guard_name'  => $guardName,
+        ]));
 
         return $role;
     }
