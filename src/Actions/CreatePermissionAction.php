bc435c0bacacea0db7c571df4241b6fc56f1022f
Misaf <misaf.1990@gmail.com>
2026-06-14 11:27:23 +0330

    Use permission model creation helper

--- src/Actions/CreatePermissionAction.php
+++ src/Actions/CreatePermissionAction.php
@@ -6,29 +6,16 @@ namespace Misaf\VendraPermission\Actions;
 
 use Misaf\VendraPermission\Models\Permission;
 use Misaf\VendraTenant\Models\Tenant;
-use Spatie\Permission\PermissionRegistrar;
 
 final class CreatePermissionAction
 {
-    public function __construct(private readonly PermissionRegistrar $permissionRegistrar) {}
-
     public function execute(
         Tenant $tenant,
         string $name,
-        ?string $description,
         string $guardName,
     ): Permission {
-        $permission = $tenant->permissions()->firstOrCreate(
-            [
-                'name'       => $name,
-                'guard_name' => $guardName,
-            ],
-            [
-                'description' => $description,
-            ],
-        );
-
-        $this->permissionRegistrar->forgetCachedPermissions();
+        /** @var Permission $permission */
+        $permission = $tenant->execute(static fn() => Permission::findOrCreate($name, $guardName));
 
         return $permission;
     }
