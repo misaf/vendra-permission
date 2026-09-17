87244f1cb9e7728dc4c75688620ab927c84bd7b4
misaf <misaf.1990@gmail.com>
2026-09-17 19:49:11 +0000

    test(store): cover the storefront:lifecycle command

--- src/Console/Commands/SeedCommand.php
+++ src/Console/Commands/SeedCommand.php
@@ -5,19 +5,19 @@ declare(strict_types=1);
 namespace Misaf\VendraPermission\Console\Commands;
 
 use Illuminate\Console\Attributes\Description;
+use Illuminate\Console\Attributes\Signature;
 use Misaf\VendraPermission\Database\Seeders\DemoContentSeeder;
 use Misaf\VendraPermission\Database\Seeders\PermissionPolicySeeder;
 use Misaf\VendraPermission\PermissionPlugin;
 use Misaf\VendraSupport\Tenancy\Console\Commands\TenantSeedCommand;
 
 #[Description('Seed permission module data for a tenant')]
+#[Signature(self::MODULE_NAME.':seed
+        {tenant? : Tenant ID or slug to seed permission data for}
+        {seeders?* : Seeder keys to run. Use "all" or one or more of: permission-policies, demo-contents}')]
 final class SeedCommand extends TenantSeedCommand
 {
     protected const string MODULE_NAME = PermissionPlugin::ID;
-
-    protected $signature = self::MODULE_NAME.':seed
-        {tenant? : Tenant ID or slug to seed permission data for}
-        {seeders?* : Seeder keys to run. Use "all" or one or more of: permission-policies, demo-contents}';
 
     /**
      * @return array<string, class-string>
